b88f77770490b248ec9a92cdac9fbf4f96461fcf
anagh-azad <anagh.azad.ug20@nsut.ac.in>
2024-05-26 16:35:54 +0530

    set up otp verification and unique username check routes

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,77 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json();
+
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsername });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found"
+        },
+        {
+          status: 404
+        }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid) {
+      if (isCodeNotExpired) {
+        user.isVerified = true;
+        await user.save();
+
+        return Response.json(
+          {
+            success: true,
+            message: "User verified successfully"
+          },
+          {
+            status: 200
+          }
+        );
+      } else {
+        return Response.json(
+          {
+            success: false,
+            message: "Verification code expired. Please sign up again!!"
+          },
+          {
+            status: 400
+          }
+        );
+      }
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Incorrect verification code"
+        },
+        {
+          status: 400
+        }
+      );
+    }
+  } catch (error) {
+    console.error("Error verifying user", error);
+
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying user"
+      },
+      {
+        status: 500
+      }
+    );
+  }
+}
